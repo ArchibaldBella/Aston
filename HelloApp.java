5b5d7f455fc3d2f6b22b917cb1979f9ae0ed0685
Artur <gaynanovbmw92@gmail.com>
2025-01-27 23:49:17 +0500

    Add files via upload

---
+++ HelloApp.java
@@ -0,0 +1,30 @@
+package helloapp;
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 4;
+        if (a > b) {
+
+            System.out.println(a > b);
+            System.out.println (a+b);
+            System.out.println (a-b);
+            System.out.println (a*b);
+            System.out.println (a/b);
+            
+        } else if (a < b) {
+            System.out.println(a < b);
+            
+        } else {
+            System.out.println(a = b);
+        
+            
+     
+        }
+        
+    }
+
+}
+    
+}
