e84ac46cc05ebc02df401c83e63cd716d9470447
Artur <gaynanovbmw92@gmail.com>
2025-01-28 00:33:57 +0500

    Add files via upload

---
+++ JavaApplication9.java
@@ -0,0 +1,21 @@
+package javaapplication9;
+
+public class JavaApplication9 {
+
+   
+public static void main(String[] args) {
+   
+    String str1 = "Hello";
+    String str2 = "hello"; {
+    
+    if (str1.equals(str2)) {
+    System.out.println("Строки равны");
+} else {
+    System.out.println("Строки не равны");
+}
+    
+        
+    }
+    
+}
+}
